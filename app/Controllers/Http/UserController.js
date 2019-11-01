248004c5d4c48aa49621adc090b82d77f6f78b23
Giovani Milani <giovanimilani11@gmail.com>
2019-11-01 12:52:25 -0300

    Session / User Controller

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,4 +1,6 @@
 'use strict'
+
+const Database = use('Database')
 const User = use('App/Models/User')
 
 class UserController {
@@ -6,9 +8,12 @@ class UserController {
     const data = request.only(['username', 'email', 'password'])
     const address = request.input('addresses')
 
-    const user = await User.create(data)
+    const trx = await Database.beginTransaction()
 
-    await user.addresses().createMany(address)
+    const user = await User.create(data, trx)
+    await user.addresses().createMany(address, trx)
+
+    await trx.commit()
 
     return user
   }
