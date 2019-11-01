010529540aebbf28be233737617614f70e04a1f9
Milani <giovanimilani11@gmail.com>
2019-11-01 07:44:42 -0300

    Tabela de endereço do Usuario + caputa das exceptions

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -4,8 +4,11 @@ const User = use('App/Models/User')
 class UserController {
   async store ({ request }) {
     const data = request.only(['username', 'email', 'password'])
+    const address = request.input('addresses')
 
     const user = await User.create(data)
+
+    await user.addresses().createMany(address)
 
     return user
   }
